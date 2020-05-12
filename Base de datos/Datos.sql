21ade7a80737c220950bd2fd20743b9f77de8735
RafaelZamora <rafael_zamora99@hotmail.com>
2020-05-12 14:34:45 -0500

    Inserción de datos
    datos

---
+++ Base de datos/Datos.sql
@@ -0,0 +1,3 @@
+insert into empleado (Dni, Nombre, Direccion, FechaNacimiento, Telefono, Usuario) values ('GOZR990823HVZNMF09', 'Rafael Antonio Gonzalez Zamora', 'Costa real #187 col. astilleros', '1999-08-23', '2295242553', 'RafaelZamora69');
+
+select * from empleado;
